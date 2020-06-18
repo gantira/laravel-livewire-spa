48a43411637ffe6e08bf91e874db0e3f6c553ddb
Angga Gantira <angga.gantira@gmail.com>
2020-06-18 10:38:11 +0700

    Tutorial CRUD Laravel Livewire SPA #4 : Install Turbolinks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Livewire SPA - SantriKoding.com</title>
-    <link rel=“stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <livewire:styles/>
     <livewire:scripts/>
 </head>
@@ -13,8 +13,25 @@
     <div class="container mt-5 mb-5">
         <div class="row justify-content-center">
             <div class="col-md-12">
+
+                <!-- flash message -->
+                @if (session()->has('message'))
+                    <div class="alert alert-success">
+                        {{ session('message') }}
+                    </div>
+                @endif
+                <!-- end flash message -->
+
+                <!-- component -->
+                @yield('content')
+                <!-- end component -->
+
             </div>
         </div>
     </div>
+
+    <!-- javascript -->
+    <script src="{{ mix('js/app.js') }}"></script>
+
 </body>
 </html>
